bdfc3a7d393a84a3365251d94b5cb8ac6a14e1ac
lip23 <liptju@163.com>
2019-11-15 00:00:02 +0800

    up 416

--- 416_partition_equal_subset_sum.cpp
+++ 416_partition_equal_subset_sum.cpp
@@ -29,3 +29,29 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    // 解题思路：
+    // 经典背包问题，动态规划
+    // dp[i][j]:前i个元素中是否存在和为j的情况
+    // dp[i][j] = dp[i][j - nums[i]]
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum & 1) {
+            return false;
+        }
+        vector<bool> dp(sum / 2 + 1, false);
+        dp[0] = true;
+        for (const auto num : nums) {
+            for (int i = dp.size() - 1; i >= 0; --i) {
+                //cout << dp[i] << " ";
+                if (dp[i] == false && i - num >=0 && dp[i - num] == true) {
+                    dp[i] = true;
+                }
+            }
+            cout <<endl;
+        }
+        return dp.back();
+    }
+};
