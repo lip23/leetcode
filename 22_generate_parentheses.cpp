768e94add7df7f2c25d38a3b21834f10311a191f
lip23 <liptju@163.com>
2019-11-22 22:10:48 +0800

    up 22 generate parentheses

--- 22_generate_parentheses.cpp
+++ 22_generate_parentheses.cpp
@@ -6,7 +6,9 @@
 class Solution {
 public:
     // 解题思路：
-    // dfs
+    // dfs, 每次递归生成一个括号，生成括号时要考虑到所以满足条件的情况
+    // 如果左括号剩余数>0，则可生成左括号
+    // 如果有括号剩余数>=左括号剩余数，则可生成右括号
     void dfs(string& pars, const int left_count, const int right_count, vector<string>& ans) {
         if (right_count == 0) {
             ans.push_back(pars);
