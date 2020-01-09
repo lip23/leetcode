0e9acc9d34133604af4dc1e816a60678a48c1613
lip23 <liptju@163.com>
2020-01-09 22:44:27 +0800

    up 5 lps

--- 5_longest_palindromic_substring.cpp
+++ 5_longest_palindromic_substring.cpp
@@ -26,3 +26,28 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    // solution
+    // dp  O(n*n)
+    // dp[i][j] 表示 s[i:j]是否回文
+    // dp[i][j] = 1     if (s[i] == s[j] && dp[i + 1][j - 1] && j - i > 1)
+    //          = 0     else
+    string longestPalindrome(string s) {
+        string ans;
+        vector<vector<int>> dp(s.length(), vector<int>(s.length(), 0));
+        for (int l = 1; l <= s.length(); ++l) {
+            for (int i = 0; i <= s.length() - l; ++i) {
+                int j = i + l - 1;
+                if (s[i] == s[j] && (l <= 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = 1;
+                    if (l > ans.length()) {
+                        ans = s.substr(i, l);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
