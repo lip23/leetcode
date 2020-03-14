26257ef39dff25748356512bff1f891751fb9bbf
lip23 <liptju@163.com>
2020-03-14 22:27:53 +0800

    up 300 lis

--- 300_longest_increasing_subsequence.cpp
+++ 300_longest_increasing_subsequence.cpp
@@ -20,3 +20,36 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    // 构造数组lis，lis[i]表示当最长递增子序列长度为i+1时，最后一个元素可以取的最小值
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis;
+        for (const auto& num : nums) {
+            if (lis.empty() || lis.back() < num) {
+                lis.push_back(num);
+                continue;
+            }
+            int beg = 0, end = lis.size() - 1;
+            while (beg < end) {
+                int mid = beg + end >> 1;
+                if (num < lis[mid]) {
+                    end = mid - 1;
+                } else if (num > lis[mid]){
+                    beg = mid + 1;
+                } {
+                    break;
+                }
+            }
+            // 当用[beg, end]（ 不是[beg,end) ）进行二分查处时，最后beg肯定会指向有效元素
+            if (lis[beg] < num) {
+                lis[beg + 1] = num;
+            } else if (lis[beg] > num) {
+                lis[beg] = num;
+            }
+        }
+
+        return lis.size();
+    }
+};
